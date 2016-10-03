21d4f51f841ddb4db8e868dba8de1c2d15904723
ukiran75 <ukiran75@gmail.com>
2016-10-03 14:05:24 -0500

    Actions File updated

--- Actions.py
+++ Actions.py
@@ -1,7 +1,26 @@
-def calForNominal(transactions,position):
-    print("record of type n and position is "+str(position))
-    return
+def createLists(transactions,position):
+    lists=[[],[]]
+    for i in range(1, len(transactions)):
+        if (transactions[i][position] == 0):
+            lists[0].append(i);
+        else:
+            lists[1].append(i);
+    return lists
 
-def calForCountinuous(transactions,position):
-    print("record of type c and position is "+str(position))
+def checkNominalAttributes(listTran,transactions):
+    countZero = 0;
+    countOne = 0;
+    for i in listTran:
+        if (transactions[i][3] == 0):
+            countZero += 1;
+        else:
+            countOne += 1;
+    if (countOne == len(listTran)):
+       return 1
+    elif (countZero == len(listTran)):
+        return 0
+    else:
+      return 2
+def checkCountinousAttributes(listTran,transactions):
+
     return
--- Decision Tree.py
+++ Decision Tree.py
@@ -1,12 +1,23 @@
 import Actions
 
-transactions=(('n','n','c','l'),(1,0,125,0),(0,1,100,0),(0,0,70,0),(1,1,120,0),(0,2,95,1),
-              (0,1,60,0),(1,2,220,0),(0,0,85,1),(0,1,75,0),(0,0,91,1))
-
-for i in range(0,len(transactions[0])):
-    if(transactions[0][i]=='n'):
-        Actions.calForNominal(transactions,i)
-    elif(transactions[0][i]=='c'):
-        Actions.calForCountinuous(transactions,i)
-    else:
-        print("record of type l")
+transactions=[('n','n','c','l'),(1,0,125,0),(0,1,100,0),(0,0,70,0),(1,1,120,0),(0,2,95,1),
+              (0,1,60,0),(1,2,220,0),(0,0,85,1),(0,1,75,0),(0,0,91,1)]
+position=0;
+tranList=[];
+lists=Actions.createLists(transactions,position)
+print(lists)
+for list in lists:
+    result=Actions.checkNominalAttributes(list,transactions)
+    print(list)
+    print(result)
+    if(result==2):
+        position+=1
+        for i in list:
+            tranList.append(transactions[i])
+        print(tranList)
+        lists2=Actions.createLists(tranList,position)
+        print(lists2)
+        for list in lists2:
+            result = Actions.checkNominalAttributes(list, transactions)
+            print(result)
+    tranList=[]
