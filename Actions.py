c3eaeac8575d0cd42dfed2d8dfa59a2adba37ca4
ukiran75 <ukiran75@gmail.com>
2016-10-03 22:18:33 -0500

    updateed recursion

--- Actions.py
+++ Actions.py
@@ -1,26 +1,39 @@
-def createLists(transactions,position):
+def createListsForNominal(transactions,position):
     lists=[[],[]]
     for i in range(1, len(transactions)):
         if (transactions[i][position] == 0):
-            lists[0].append(i);
+            lists[0].append(i)
         else:
-            lists[1].append(i);
+            lists[1].append(i)
     return lists
 
-def checkNominalAttributes(listTran,transactions):
-    countZero = 0;
-    countOne = 0;
+def createListsForContinuous(transactions,position):
+    print(transactions)
+    print(position)
+    lists=[[],[]]
+    sum=0;count=0
+    for i in range(1, len(transactions)):
+        sum+=transactions[i][position]
+        count+=1
+    sum=sum/count
+    for i in range(1, len(transactions)):
+        if (transactions[i][position] <= 80):
+            lists[0].append(i)
+        else:
+            lists[1].append(i)
+    return lists
+
+def checkAttributes(listTran,position,transactions):
+    countZero = 0
+    countOne = 0
     for i in listTran:
         if (transactions[i][3] == 0):
-            countZero += 1;
+            countZero += 1
         else:
-            countOne += 1;
+            countOne += 1
     if (countOne == len(listTran)):
        return 1
     elif (countZero == len(listTran)):
         return 0
     else:
       return 2
-def checkCountinousAttributes(listTran,transactions):
-
-    return
--- Decision Tree.py
+++ Decision Tree.py
@@ -1,23 +1,34 @@
 import Actions
 
-transactions=[('n','n','c','l'),(1,0,125,0),(0,1,100,0),(0,0,70,0),(1,1,120,0),(0,2,95,1),
-              (0,1,60,0),(1,2,220,0),(0,0,85,1),(0,1,75,0),(0,0,91,1)]
+TransactionsData=[('n','n','c','l'),(1,1,125,0),(0,0,100,0),(0,1,70,0),(1,0,120,0),(0,2,95,1),
+              (0,0,60,0),(1,2,220,0),(0,1,85,1),(0,0,75,0),(0,1,91,1)]
+decisionTree=[];
 position=0;
 tranList=[];
-lists=Actions.createLists(transactions,position)
-print(lists)
-for list in lists:
-    result=Actions.checkNominalAttributes(list,transactions)
-    print(list)
-    print(result)
-    if(result==2):
-        position+=1
-        for i in list:
-            tranList.append(transactions[i])
-        print(tranList)
-        lists2=Actions.createLists(tranList,position)
-        print(lists2)
-        for list in lists2:
-            result = Actions.checkNominalAttributes(list, transactions)
-            print(result)
-    tranList=[]
+iTransData=TransactionsData;
+def checkResult(res,transList,position):
+    iTransData=[];
+    if(res==2 and position<4):
+        for i in transList:
+            iTransData.append(TransactionsData[i])
+        create(iTransData,position+1)
+    elif(res==1):
+        print('decision made')
+    else:
+        print('decision made')
+
+    return
+
+def create(transactionsData,position):
+    if(transactionsData[0][position]=='n'):
+        lists = Actions.createListsForNominal(iTransData, position)
+        for tranList in lists:
+            result=Actions.checkAttributes(tranList,position,transactionsData)
+            checkResult(result,tranList,position)
+    elif(transactionsData[0][position]=='c'):
+        lists = Actions.createListsForContinuous(iTransData, position)
+        for tranList in lists:
+            result=Actions.checkAttributes(tranList,position,transactionsData)
+            checkResult(result,tranList,position)
+create(TransactionsData,0)
+
