87ed5125908475bad43fc872165cfe97bb07f40d
ukiran75 <ukiran75@gmail.com>
2016-11-28 21:10:19 -0600

    New code

--- Hunt's Algorithm.py
+++ Hunt's Algorithm.py
@@ -14,8 +14,9 @@ decisionTree=[]
 defzeroCount = 0
 defoneCount = 0
 defVal=0;
-label=['No','Yes','NA',defVal]
+label=['No','Yes','NA']
 
+# calculating the default value of the label from the data
 for data in transactionData[1:]:
      if (data[length] == 0):
           defzeroCount += 1
@@ -23,14 +24,14 @@ for data in transactionData[1:]:
          defoneCount += 1
 
 if (defzeroCount > defoneCount):
-  defVal=0
+  defVal=0;
 elif (defzeroCount < defoneCount):
   defVal=1
 
 
-
+#calculating the seperator value for node traversing
 def calculateSeparatorValue(position):
-    max=-1;min=100000000000
+    max=-10000000000;min=100000000000
     for transaction  in transactionData[1:]:
         if(transaction[position]<min):
             min=transaction[position]
@@ -38,6 +39,7 @@ def calculateSeparatorValue(position):
             max=transaction[position]
     return (max+min)/2
 
+#checking the purity of the node
 def checkPurity(checkData):
     zeroCount=0;oneCount=0
     if(len(checkData)==0):
@@ -55,6 +57,7 @@ def checkPurity(checkData):
         return 2
 
 
+#calculating the maximum lable value if there are no attribute to traverse
 def calculateMaximun(checkData):
     zeroCount = 0;
     oneCount = 0
@@ -70,15 +73,12 @@ def calculateMaximun(checkData):
     else:
         return defVal
 
-
-
+#inserting nodes into the decision tree
 def insertIntoTree(purity,position,seperatorvalue,leaf,side,nodeData=[]):
       node=[leaf,label[purity],position,seperatorvalue,side]
       decisionTree.append(node)
 
-
-
-
+#creating the nodes recursively
 def createNodes(nodeData,position):
     seperatotValue=calculateSeparatorValue(position)
     end=0;
@@ -94,33 +94,60 @@ def createNodes(nodeData,position):
     leftNodePurity=checkPurity(leftNodeData)
     rightNodePurity=checkPurity(rightNodeData)
     if(leftNodePurity==0 or leftNodePurity==1):
-        insertIntoTree(leftNodePurity,position,seperatotValue,"Yes","left")
+        insertIntoTree(leftNodePurity,position,seperatotValue,"Yes","left") #if the node is pure that is all the node data having the same label
     elif(leftNodePurity!=-1):
         if(end==1):
             #print("No attribute to traverse")
-            maxLeafVal=calculateMaximun(leftNodeData)
+            maxLeafVal=calculateMaximun(leftNodeData) #if the node is not pure but we have no attributes to traverse we use the default max label value of the data
             insertIntoTree(maxLeafVal, position, seperatotValue, "Yes", "left")
         else:
-            insertIntoTree(leftNodePurity, position, seperatotValue, "No", "left",leftNodeData)
+            insertIntoTree(leftNodePurity, position, seperatotValue, "No", "left",leftNodeData)#if data is not pure insert it as a parent node and do recursion
             createNodes(leftNodeData,position+1)
     else:
-        insertIntoTree(3, position, seperatotValue, "Yes", "left")
+        insertIntoTree(defVal, position, seperatotValue, "Yes", "left")#if  there is no data for the node insert the default node with label value as maxlabel value
 
     if (rightNodePurity == 0 or rightNodePurity == 1):
-        insertIntoTree(rightNodePurity, position, seperatotValue, "Yes", "right")
+        insertIntoTree(rightNodePurity, position, seperatotValue, "Yes", "right") #if the node is pure that is all the node data having the same label
     elif (rightNodePurity != -1):
         if (end == 1):
            # print("No attribute to traverse")
-           maxLeafVal = calculateMaximun(rightNodeData)
+           maxLeafVal = calculateMaximun(rightNodeData)#if the node is not pure but we have no attributes to traverse we use the default max label value of the data
            insertIntoTree(maxLeafVal, position, seperatotValue, "Yes", "right")
         else:
-            insertIntoTree(rightNodePurity, position, seperatotValue, "No", "right",rightNodeData)
+            insertIntoTree(rightNodePurity, position, seperatotValue, "No", "right",rightNodeData)#if data is not pure insert it as a parent node and do recursion
             createNodes(rightNodeData, position + 1)
     else:
-        insertIntoTree(3,position,seperatotValue,"Yes","right")
+        insertIntoTree(defVal,position,seperatotValue,"Yes","right")#if  there is no data for the node insert the default node with label value as maxlabel value
 
 createNodes(transactionData[1:],0)
 
+#printing the nodes of the decision tree including the default nodes
 print("[isLeaf,lableValue,attributePos,seperatorvalue,nodeSide]");
 for node in decisionTree:
   print(node)
+
+fromPos=0
+#traversing the tree for ouput
+def traverse(input,position=0,fromPos=0):
+    for node in decisionTree[fromPos:]:
+        fromPos+=1
+        if(node[2]==position):
+            if ((input[position] <= node[3]) and (node[4] == 'left')):
+               if (node[0] == "Yes"):
+                print(node[1])
+               else:
+                traverse(input, position + 1,fromPos)
+            elif((input[position]>node[3]) and (node[4]=='right')):
+                if(node[0]=="Yes"):
+                    print(node[1])
+                else:
+                    traverse(input,position+1,fromPos)
+
+
+traverse([1,2,220])#change the list to different transaction
+
+
+
+
+
+
