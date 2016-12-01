16ed81370d1b70d8e6f6a190d8aa0837794e455e
ukiran75 <ukiran75@gmail.com>
2016-12-01 13:32:05 -0600

    Actions File updated

--- Hunt's Algorithm using GINI.py
+++ Hunt's Algorithm using GINI.py
@@ -1,3 +1,4 @@
+
 '''
 Course     : Data Mining
 Professor  : Dr Hung Chi Su
@@ -9,20 +10,10 @@ Home Work# : HW5(Hunts Algorithm Implementation)
 
 import math
 import sys
-transactionData=[('n','o','c','l'),(1,0,125,0),(0,1,100,0),(0,0,70,0),(1,1,120,0),(0,2,95,1),
+transactionData=[('n','n','c','l'),(1,0,125,0),(0,1,100,0),(0,0,70,0),(1,1,120,0),(0,2,95,1),
               (0,1,60,0),(1,2,220,0),(0,0,85,1),(0,1,75,0),(0,0,90,1)]
 tree=[]
-sys.setrecursionlimit(1500)
-def calculateSeparatorValue(position):
-    max=-10000000000;min=100000000000
-    for transaction  in transactionData[1:]:
-        if(transaction[position]<min):
-            min=transaction[position]
-        elif(transaction[position] > max):
-            max=transaction[position]
-    return (max+min)/2
-
-
+#calculating ones in the node for gini calculation
 def calculateOnes(NodeData):
     length=len(transactionData[0])
     onesCount=0
@@ -31,6 +22,7 @@ def calculateOnes(NodeData):
             onesCount+=1
     return onesCount
 
+#Method to calculate gini for ordinal attributes
 def calculateGiniOrdinal(position,nodeTransactions):
     uniqVal=[]
     bestGini = 10
@@ -42,13 +34,14 @@ def calculateGiniOrdinal(position,nodeTransactions):
             uniqVal.append(transaction[position])
     for val in uniqVal:
         values=calGini(position,nodeTransactions,val,1)
-        if values[0] < bestGini:
+        if values[0] < bestGini:# if the gini for a split is better than the other split in atrribute make that as the best split
             bestGini=values[0]
             bestSeperatorValue=values[1]
             bestLeftNode=values[2]
             bestRightNode=values[3]
     return [bestGini, bestSeperatorValue, bestLeftNode, bestRightNode]
 
+#method to calculate the gini
 def calGini(position,nodeTransactions,seperatorValue,ordinal=0):
     leftNodeData = []
     rightNodeData = []
@@ -81,15 +74,29 @@ def calGini(position,nodeTransactions,seperatorValue,ordinal=0):
     return [gini,seperatorValue,leftNodeData,rightNodeData]
 
 
-
+#Method to calculate gini for nominal attributes
 def calculateGiniNominal(position,nodeTransactions):
-    seperatorValue=calculateSeparatorValue(position)
-    return calGini(position,nodeTransactions,seperatorValue)
+    uniqVal = []
+    bestGini = 10
+    bestSeperatorValue = 0
+    bestLeftNode = []
+    bestRightNode = []
+    for transaction in nodeTransactions:
+        if transaction[position] not in uniqVal:
+            uniqVal.append(transaction[position])
+    for val in uniqVal:
+        values = calGini(position, nodeTransactions, val, 0)
+        if values[0] < bestGini:# if the gini for a split is better than the other split in atrribute make that as the best split
+            bestGini = values[0]
+            bestSeperatorValue = values[1]
+            bestLeftNode = values[2]
+            bestRightNode = values[3]
+    return [bestGini, bestSeperatorValue, bestLeftNode, bestRightNode]
 
-
+#Method to calculate gini for continuous attributes
 def calculateGiniContinuous(position,nodeTransactions):
     values=[]
-    for transaction in nodeTransactions:
+    for transaction in nodeTransactions: #getting all the continuous values for iterating to find the best split
         values.append(transaction[position])
     values.sort()
     initial=values[0]-10
@@ -98,10 +105,10 @@ def calculateGiniContinuous(position,nodeTransactions):
     bestLeftNode = []
     bestRightNode = []
     for value in values:
-        seperatorValue=(initial+value)/2
+        seperatorValue=(initial+value)/2 #getting the average for the previous value and current value for finding split value
         initial=value
         values=calGini(position,nodeTransactions,seperatorValue)
-        if values[0] <= bestGini:
+        if values[0] < bestGini:# if the gini for a split is better than the other split in atrribute make that as the best split
             bestGini=values[0]
             bestSeperatorValue=values[1]
             bestLeftNode=values[2]
@@ -110,7 +117,7 @@ def calculateGiniContinuous(position,nodeTransactions):
 
 
 
-
+#calling the calculate gini based on the attribute type
 def calculateGini(position,nodeTransactions):
     attributeType=transactionData[0][position]
     values=[]
@@ -122,7 +129,7 @@ def calculateGini(position,nodeTransactions):
         values = calculateGiniOrdinal(position, nodeTransactions)
     return values
 
-
+# calculating the default value of the label from the data
 def calculateDefault(checkData):
     zeroCount = 0
     oneCount = 0
@@ -136,7 +143,7 @@ def calculateDefault(checkData):
         return 0
     else:
         return 1
-
+#checking the purity of the node
 def checkPurity(checkData):
     zeroCount=0;oneCount=0
     length = len(transactionData[0]) - 1
@@ -154,161 +161,144 @@ def checkPurity(checkData):
     else:
         return 2
 
+#getting the node value
+def getNodevalue(lOr,isleaf,attrposition,splitValue,label):
+    value=[]
+    value.append(lOr)
+    value.append(isleaf)
+    value.append(attrposition)
+    value.append(splitValue)
+    value.append(label)
+    return value
+
+#calculating the bestgini for the attributes in the nodeData
 def calculateBestGini(nodeTransactions,omitAttributes):
-    if(len(omitAttributes)>3):
-        default=calculateDefault(nodeTransactions)
+    if(len(omitAttributes)>3):#continue if we didnt split all the attribute(ie no attributes left to split)
+        default=calculateDefault(nodeTransactions)#if so make the a default node
         print("Defaut Node:"+str(default))
         return
     bestgini=10
     bestPos=-1
     nextSplitValues=[]
-    for position in range(0,(len(transactionData[0])-1)):
+    for position in range(0,(len(transactionData[0])-1)):#calculating the best gini split from the attributes left
         if position not in omitAttributes:
             values=calculateGini(position,nodeTransactions)
             if(len(values)>0):
                 gini=values[0]
-                #print("gini:"+str(gini))
-                if gini < bestgini:
+                print("gini:"+str(gini))
+                if gini <= bestgini:
                     bestgini=gini
                     bestPos=position
                     nextSplitValues=values
-    #print(nextSplitValues)
-    omitAttributes.append(bestPos)
-    #print("omitAttributes:"+str(omitAttributes))
+    omitAttributes.append(bestPos)#if we find an attribute having best gini split omit that attribute for furthur splitting
     leftFlag=0;rightFlag=0
     if(len(omitAttributes)<=3):
-       #print(len(omitAttributes))
        if (len(nextSplitValues[2])!=0):
           #print("Inside left split")
-          purity=checkPurity(nextSplitValues[2])
-          if(purity==2 and len(omitAttributes)<3 ):
+          purity=checkPurity(nextSplitValues[2])#check if the node id pure
+          if(purity==2 and len(omitAttributes)<3 ):#if the node is not pure we still have attributes to split recurse the calculateBestGini()
               print("Left Node Needs Splitting(Position="+str(bestPos)+"),Split Value=("+str(nextSplitValues[1])+")")
-              nodeVal = []
-              nodeVal.append(0)
-              nodeVal.append(1)
-              nodeVal.append(bestPos)
-              nodeVal.append(nextSplitValues[1])
-              nodeVal.append(0)
+              nodeVal = getNodevalue(0,0,bestPos,nextSplitValues[1],0)
               tree.append(nodeVal)
-              #tree.append("("+str(0)+","+str(0)+","+str(bestPos)+","+str(nextSplitValues[1])+","+str(0)+")")
               leftFlag=1
-              #calculateBestGini(nextSplitValues[2],omitAttributes)
-          elif (len(omitAttributes)==3 and purity==2):
+          elif (len(omitAttributes)==3 and purity==2):#if the node is not pure we do not  have attributes to split calculate the default node
               default = calculateDefault(nodeTransactions)
-              nodeVal = []
-              nodeVal.append(0)
-              nodeVal.append(1)
-              nodeVal.append(bestPos)
-              nodeVal.append(nextSplitValues[1])
-              nodeVal.append(default)
+              nodeVal = nodeVal = getNodevalue(0,1,bestPos,nextSplitValues[1],default)
               tree.append(nodeVal)
-              #tree.append("(" + str(0) + "," + str(1) + "," + str(bestPos) + "," + str(nextSplitValues[1]) + "," + str(default) + ")")
-              print("default left:" + str(default))
-          else:
-              #tree.append("(" + str(0) + "," + str(1) + "," + str(bestPos) + "," + str(nextSplitValues[1]) + "," + str(purity) + ")")
-              nodeVal = []
-              nodeVal.append(0)
-              nodeVal.append(1)
-              nodeVal.append(bestPos)
-              nodeVal.append(nextSplitValues[1])
-              nodeVal.append(purity)
+              print("default left(No attribute to split):" + str(default))
+          else:#if the  node is pure
+              nodeVal = getNodevalue(0,1,bestPos,nextSplitValues[1],purity)
               tree.append(nodeVal)
               print("LeftNode Pure with value:"+str(purity))
        else:
-           default = calculateDefault(nodeTransactions)
-           nodeVal = []
-           nodeVal.append(0)
-           nodeVal.append(1)
-           nodeVal.append(bestPos)
-           nodeVal.append(nextSplitValues[1])
-           nodeVal.append(default)
+           default = calculateDefault(nodeTransactions)#if the node do not  have any data to split calculate the default node
+           nodeVal = nodeVal = getNodevalue(0, 1, bestPos, nextSplitValues[1], default)
            tree.append(nodeVal)
-           #tree.append("(" + str(0) + "," + str(1) + "," + str(bestPos) + "," + str(nextSplitValues[1]) + "," + str(default) + ")")
-           print("default left:" + str(default))
+           print("default left(No node data):" + str(default))
 
 
        if (len(nextSplitValues[3])!=0):
          # print("Inside right split")
-          purity=checkPurity(nextSplitValues[3])
-          if(purity==2 and len(omitAttributes)<3):
+          purity=checkPurity(nextSplitValues[3])#check if the node id pure
+          if(purity==2 and len(omitAttributes)<3):#if the node is not pure we still have attributes to split recurse the calculateBestGini()
               print("Right Node Needs Splitting(Position="+str(bestPos)+"),Split Value=("+str(nextSplitValues[1])+")")
-              #tree.append("(" + str(1) + "," + str(0) + "," + str(bestPos) + "," + str(nextSplitValues[1]) + "," + str(0) + ")")
-              nodeVal = []
-              nodeVal.append(1)
-              nodeVal.append(0)
-              nodeVal.append(bestPos)
-              nodeVal.append(nextSplitValues[1])
-              nodeVal.append(0)
+              nodeVal = getNodevalue(1, 0, bestPos, nextSplitValues[1], 0)
               tree.append(nodeVal)
               rightFlag = 1
-              #calculateBestGini(nextSplitValues[3],omitAttributes)
-          elif (len(omitAttributes)==3 and purity==2):
+          elif (len(omitAttributes)==3 and purity==2):#if the node is not pure we do not  have attributes to split calculate the default node
               default = calculateDefault(nodeTransactions)
-              nodeVal=[]
-              nodeVal.append(1)
-              nodeVal.append(1)
-              nodeVal.append(bestPos)
-              nodeVal.append(nextSplitValues[1])
-              nodeVal.append(default)
+              nodeVal = nodeVal = getNodevalue(1, 1, bestPos, nextSplitValues[1], default)
               tree.append(nodeVal)
-              #tree.append("(" + str(1) + "," + str(1)+ "," + str(bestPos) + "," + str(nextSplitValues[1]) + "," + str(default) + ")")
-              print("default right:" + str(default))
-          else:
-              nodeVal = []
-              nodeVal.append(1)
-              nodeVal.append(1)
-              nodeVal.append(bestPos)
-              nodeVal.append(nextSplitValues[1])
-              nodeVal.append(purity)
+              print("default Right(No attribute to split):" + str(default))
+          else:#if the  node is pure
+              nodeVal = nodeVal = getNodevalue(1, 1, bestPos, nextSplitValues[1], purity)
               tree.append(nodeVal)
-              #tree.append("(" + str(1) + "," + str(1) + "," + str(bestPos) + "," + str(nextSplitValues[1]) + "," + str(purity) + ")")
               print("RightNode Pure with value:"+str(purity))
        else:
-           default = calculateDefault(nodeTransactions)
-           nodeVal = []
-           nodeVal.append(1)
-           nodeVal.append(1)
-           nodeVal.append(bestPos)
-           nodeVal.append(nextSplitValues[1])
-           nodeVal.append(default)
+           default = calculateDefault(nodeTransactions)#if the node do not  have any data to split calculate the default node
+           nodeVal = nodeVal = getNodevalue(1, 1, bestPos, nextSplitValues[1], default)
            tree.append(nodeVal)
-           #tree.append("(" + str(1) + "," + str(1) + "," + str(bestPos) + "," + str(nextSplitValues[1]) + "," + str(default) + ")")
-           print("default right:" + str(default))
-    if(leftFlag==1):
+           print("default left(No node data)" + str(default))
+    if(leftFlag==1):# if the leftnode is not pure call recursion
         calculateBestGini(nextSplitValues[2], omitAttributes)
-    if (rightFlag == 1):
+    if (rightFlag == 1):# if the rightnode is not pure call recursion
         calculateBestGini(nextSplitValues[3], omitAttributes)
 
-print("Node Values(Left(0)OrRight(1),isLeaf,AttributePosition,SeperatorValue,Label)")
 
 
+stop=0
 def traverse(input,i):
-    while i < len(tree) :
+    global stop
+    while (i < len(tree)) and (stop==0) :
         attPos = tree[i][2]
-        if(input[attPos]<=tree[i][3]):
-            if(tree[i][0]==1):
-                print(tree[i][4])
+        flag=0
+        flag2=0
+        if(tree[i][0]==1):
+            flag2=1
+        if(transactionData[0][attPos]=='o'):
+            flag=1
+        if(input[attPos]<=tree[i][3] and flag==0 and flag2==0):
+            if(tree[i][1]==1):
+                print('\nThe class label for input is: '+str(tree[i][4]))
+                stop=1
             else:
-                if(tree[i+1][0]==1):
-                    i=+2
+                if(tree[i+1][1]==1):
+                    i+=2
                     traverse(input,i)
                 else:
                     i+=3
                     traverse(input, i)
-        else:
-            i=i+1
-            if (tree[i][0] == 1):
+        elif (input[attPos] == tree[i][3] and flag==1 and flag2==0):
+            if (tree[i][1] == 1):
                 print(tree[i][4])
+                exit(1)
             else:
-                if (tree[i - 1][0] == 1):
-                    i = +1
+                if (tree[i + 1][1] == 1):
+                    i += 2
                     traverse(input, i)
                 else:
-                    i+=2
-                    traverse(input,i)
+                    i += 3
+                    traverse(input, i)
+        else:
+            i=i+1
+            if(flag2==1):
+                i=i-1
+            if (i<len(tree)):
+                if (tree[i][1] == 1):
+                    print('\nThe class label for input is: ' + str(tree[i][4]))
+                    stop=1
+                else:
+                    if (tree[i - 1][1] == 1):
+                        i  +=1
+                        traverse(input, i)
+                    else:
+                        i+=2
+                        traverse(input,i)
 
 
 calculateBestGini(transactionData[1:],[])
+print("\n*****************************************Tree*****************************************")
+print("Node Values Layout:  (Left(0)OrRight(1),isLeaf,AttributePosition,SeperatorValue,Label) \n")
 print(str(tree))
-
-traverse([0,1,150],0)
+traverse([1,2,90],0)
+print("\n**************************************************************************************")
